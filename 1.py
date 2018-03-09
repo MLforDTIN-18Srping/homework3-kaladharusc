62c77b5450d7f78c4397c3b4a3688bab831fb837
kaladharusc <mummadi@usc.edu>
2018-03-09 10:45:12 -0800

    first commit

---
+++ 1.py
@@ -0,0 +1,275 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
+from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
+import xgboost
+from sklearn.model_selection import KFold
+##################################################### Common Functions #################################################
+plt.style.use('ggplot')
+not_predictors = ["state", "county", "community", "communityname"]
+response_var = "ViolentCrimesPerPop"
+def plot_corr(correlations, columns):
+    fig = plt.figure(figsize=(30,30))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(correlations, vmin=-1, vmax=1)
+    fig.colorbar(cax)
+    ticks = np.arange(0, len(columns), 1)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_xticklabels(columns)
+    ax.set_yticklabels(columns)
+    plt.xticks(rotation=90)
+    plt.savefig("./plots/1_c_correlations.png")
+def scatterplot(X, Y, xLabel, yLabel, xticks=None, yticks=None):
+    plt.scatter(X, Y, s=2)
+    plt.xlabel(xLabel)
+    if xticks is not None:
+        plt.xticks(np.arange(xticks.size), xticks)
+    if yticks is not None:
+        plt.yticks(np.arange(yticks.size), yticks)
+    plt.ylabel(yLabel)
+
+##################################################### Common Functions #################################################
+#=======================================================================================================================
+def a_read_data():
+    data_df = pd.read_csv("communities.data", skip_blank_lines=False)
+    print(data_df.shape)
+#=======================================================================================================================
+def b_fill_nan():
+    data_df = pd.read_csv("communities.data", skip_blank_lines=False)
+    data_df[data_df.columns] = data_df[data_df.columns].replace(to_replace='?', value=np.NaN)
+    data_df = data_df.apply(pd.to_numeric, errors='ignore')
+    data_df.fillna(data_df.mean(), inplace=True)
+    #print(data_df.isnull().sum())
+    global not_predictors
+    data_df = data_df.drop(not_predictors, axis=1)
+    return data_df[:1495],data_df[1495:], data_df
+#=======================================================================================================================
+def c_correlation_matrix():
+    train_data, test_data,_ = b_fill_nan()
+    correlations = train_data.corr()
+    print(train_data.columns.values)
+    plot_corr(correlations, train_data.columns.values)
+#=======================================================================================================================
+def d_coefficient_variation():
+    train_data, test_data,_ = b_fill_nan()
+    mean_values = train_data.mean()
+    std_values = train_data.std()
+    CV = std_values/mean_values
+    CV = CV.sort_values(ascending=False)
+
+    print(CV)
+    return CV
+#=======================================================================================================================
+def e_plots():
+    CV_series = d_coefficient_variation()
+    train_data, test_data,_ = b_fill_nan()
+    top_cvs = CV_series.head(11)
+    Y = train_data[response_var]
+    fig = plt.figure(figsize=(20, 10))
+    box_plot_data = train_data[top_cvs.index]
+    plt.boxplot(box_plot_data.as_matrix(), labels=top_cvs.index.values)
+    plt.savefig("./plots/e_boxplot.png")
+    plt.close()
+
+    fig = plt.figure(figsize=(15, 20))
+    i = 1
+    for (key,value) in top_cvs.items():
+        fig.add_subplot(5,2,i)
+        X = train_data[key]
+        scatterplot(X,Y, key, response_var)
+        i += 1
+    plt.savefig("./plots/e_scatter_plots.png")
+    plt.close()
+#=======================================================================================================================
+def f_linear_regression():
+    train_data, test_data,_ = b_fill_nan()
+
+    y_train = train_data[response_var]
+    x_train = train_data.drop(response_var, axis=1)
+
+    y_test = test_data[response_var]
+    x_test = test_data.drop(response_var,axis=1)
+
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    y_predict = lr.predict(x_test)
+
+    mse = mean_squared_error(y_test, y_predict)
+    print(mse)
+
+def g_ridge_regression():
+    train_data, test_data,_ = b_fill_nan()
+
+    y_train = train_data[response_var]
+    x_train = train_data.drop(response_var, axis=1)
+
+    y_test = test_data[response_var]
+    x_test = test_data.drop(response_var,axis=1)
+
+    ridge_regression = RidgeCV(alphas=[0.001,0.01,0.1,1.0,10.0],cv=None, store_cv_values=True)
+    ridge_regression.fit(x_train, y_train)
+
+    print("selected lambda", ridge_regression.alpha_)
+    y_predict = ridge_regression.predict(x_test)
+
+    mse = mean_squared_error(y_test, y_predict)
+    print(mse)
+
+def h_lasso_regresion():
+    train_data, test_data, _ = b_fill_nan()
+
+    y_train = train_data[response_var]
+    x_train = train_data.drop(response_var, axis=1)
+
+    y_test = test_data[response_var]
+    x_test = test_data.drop(response_var, axis=1)
+
+    alphas = [-0.0001,-0.001, -0.01, -0.1, -1.0,-10.0, 0.001, 0.01, 0.1, 1.0, 10.0]
+
+    lasso_regression = LassoCV(alphas=alphas, cv=None,max_iter=100)
+    lasso_regression.fit(x_train, y_train)
+
+    print("selected lambda", lasso_regression.alpha_)
+    coeff_df = pd.DataFrame([lasso_regression.coef_], columns=train_data.columns.values[:-1]).transpose()
+    coeff_df = coeff_df[coeff_df[0] != 0.0]
+    print(coeff_df)
+
+    y_predict = lasso_regression.predict(x_test)
+    print("mean squared error", mean_squared_error(y_test, y_predict))
+
+    lasso_regression_norm = LassoCV(alphas=alphas, cv=None, max_iter=100, normalize=True)
+    lasso_regression_norm.fit(x_train, y_train)
+
+    print("selected lambda norm", lasso_regression_norm.alpha_)
+    coeff_df = pd.DataFrame([lasso_regression_norm.coef_], columns=train_data.columns.values[:-1]).transpose()
+    coeff_df = coeff_df[coeff_df[0] != 0.0]
+    print(coeff_df)
+
+    y_predict_nrom = lasso_regression_norm.predict(x_test)
+    print("mean squared error norm", mean_squared_error(y_test, y_predict_nrom))
+
+def i_pcr():
+    train_data, test_data, _ = b_fill_nan()
+
+    y_train = train_data[response_var]
+    x_train = train_data.drop(response_var, axis=1)
+
+    y_test = test_data[response_var]
+    x_test = test_data.drop(response_var, axis=1)
+
+    components = np.arange(1,min(train_data.shape), 2)
+
+
+    cv_scores = []
+    test_errors = []
+    for n in components:
+        pca = PCA(n_components=n)
+        cv_scores.append(np.mean(cross_val_score(pca,x_train, y_train, cv=5)))
+        x_pca = pca.fit_transform(x_train)
+        x_test_pca = pca.fit_transform(x_test)
+
+        lr = LinearRegression()
+        lr.fit(x_pca, y_train)
+
+        y_predict = lr.predict(x_test_pca)
+        test_errors.append(mean_squared_error(y_test, y_predict))
+
+    #print(cv_scores)
+    max_val = max(cv_scores)
+    index = cv_scores.index(max_val)
+
+    # pca = PCA(n_components=components[index], svd_solver="full")
+    # x_pca = pca.fit_transform(x_train)
+    #
+    # x_test_pca = pca.fit_transform(x_test)
+    # lr = LinearRegression()
+    # lr.fit(x_pca,y_train)
+
+    #y_predict = lr.predict(x_test_pca)
+    # print(mean_squared_error(y_test, y_predict))
+
+    min_val = min(test_errors)
+    index_te = test_errors.index(min_val)
+    print(components[index_te], min_val)
+    plt.plot(components, test_errors, linestyle='--', marker='o', ms=3)
+    plt.xlabel("n components")
+    plt.ylabel("MSE")
+    # plt.plot(components[index], cv_scores[index],marker='X', ms=6)
+    # print(components[index], cv_scores[index])
+    plt.savefig("./i_pcr_mse.png")
+    plt.close()
+
+    plt.plot(components, cv_scores, linestyle='--', marker='o', ms=3)
+    plt.plot(components[index], cv_scores[index],marker='X', ms=6)
+    print(components[index], cv_scores[index])
+    plt.xlabel("n components")
+    plt.ylabel("CV scores")
+    plt.savefig("./i_pcr_cv_score.png")
+
+
+
+def j_xg_boost():
+    train_data, test_data, _ = b_fill_nan()
+
+    y_train = train_data[response_var]
+    x_train = train_data.drop(response_var, axis=1)
+
+    y_test = test_data[response_var]
+    x_test = test_data.drop(response_var, axis=1)
+
+
+
+
+    cv_scores = []
+    cv_test_scores = []
+    i = 3
+    cv_train_scores = []
+    alphas = [0.0001, 0.001, 0.01, 0.1, 0.2,0.3]
+    n_trees = range(1,20,1)
+
+    for n in n_trees:
+        print(n)
+        model = xgboost.XGBRegressor(max_depth=n, learning_rate=0.1)
+        folds = KFold(n_splits=5,random_state=7)
+        result = np.mean(cross_val_score(model, x_train, y_train, cv=folds))
+        model.fit(x_train,y_train)
+        y_predict = model.predict(x_test)
+        y_train_predict = model.predict(x_train)
+        cv_test_scores.append(mean_squared_error(y_test,y_predict))
+        cv_train_scores.append(mean_squared_error(y_train, y_train_predict))
+        cv_scores.append(result)
+
+    cv_scores = np.array(cv_scores)
+    cv_test_scores = np.array(cv_test_scores)
+
+    plt.errorbar(alphas, cv_scores, label=str(i)+"CV train")
+    # plt.errorbar(alphas, cv_test_scores, label=str(i)+" Mean Squared Test data")
+    # plt.errorbar(alphas, cv_train_scores, label=str(i) + " Mean Squared Train data")
+
+    plt.xlabel("regularization parameter")
+    plt.legend(loc="upper right")
+    plt.savefig("./plots/1_j_xgboost_alpha_reg_parameter_final.png")
+
+
+if __name__ == '__main__':
+
+    # a_read_data()
+    # b_fill_nan()
+    #c_correlation_matrix()
+    #d_coefficient_variation()
+    #e_plots()
+    #f_linear_regression()
+    #g_ridge_regression()
+    #h_lasso_regresion()
+    #i_pcr()
+    j_xg_boost()
+
+
+
+
+
+
---
+++ 2.py
@@ -0,0 +1,316 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
+import weka.core.jvm as jvm
+import itertools
+##################################################### Common Functions #################################################
+def read_data():
+    train_data = pd.read_csv("aps_failure_training_set.csv", skip_blank_lines=False)
+    test_data = pd.read_csv("aps_failure_test_set.csv", skip_blank_lines=False)
+
+    return train_data,test_data
+
+def plot_corr(correlations, columns):
+    fig = plt.figure(figsize=(30,30))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(correlations, vmin=-1, vmax=1)
+    fig.colorbar(cax)
+    ticks = np.arange(0, len(columns), 1)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_xticklabels(columns)
+    ax.set_yticklabels(columns)
+    plt.xticks(rotation=90)
+    plt.savefig("./plots/2_b_iii_correlations.png")
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+def plot_roc_curve(y_true, y_predict_scores, charname):
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    #for i in range(n_classes):
+    fpr[2], tpr[2], _ = roc_curve(y_true, y_predict_scores, pos_label="pos", drop_intermediate=False)
+    roc_auc[2] = auc(fpr[2], tpr[2])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_predict_scores.ravel(), pos_label="pos")
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.savefig(charname)
+
+##################################################### Common Functions #################################################
+# =======================================================================================================================
+def b_i_impute_data():
+    train_data, test_data = read_data()
+    train_data[train_data.columns] = train_data[train_data.columns].replace(to_replace='na', value=np.NaN)
+    test_data[test_data.columns] = test_data[test_data.columns].replace(to_replace='na', value=np.NaN)
+    test_data = test_data.apply(pd.to_numeric, errors='ignore')
+    train_data = train_data.apply(pd.to_numeric, errors='ignore')
+    train_data.fillna(train_data.mean(), inplace=True)
+    test_data.fillna(test_data.mean(), inplace=True)
+
+    return train_data, test_data
+# ======================================================================================================================
+def b_ii_coefficient_variation():
+    train_data, test_data = b_i_impute_data()
+    mean_values = train_data.mean()
+    std_values = train_data.std()
+    CV = std_values/mean_values
+    CV = CV.sort_values(ascending=False)
+
+    print(CV)
+    return CV
+# ======================================================================================================================
+def b_iii_correlation_matrix():
+    train_data, test_data = b_i_impute_data()
+    train_data = train_data.drop("class", axis=1)
+    correlations = train_data.corr()
+    plot_corr(correlations, train_data.columns.values)
+# ======================================================================================================================
+def b_iv_plots():
+    train_data, test_data = b_i_impute_data()
+
+# ======================================================================================================================
+def b_v_numberof_pos_neg():
+    train_data, test_data = b_i_impute_data()
+    classes = train_data["class"]
+
+    print(classes.value_counts())
+# ======================================================================================================================
+
+
+def c_random_forest():
+    train_data, test_data = b_i_impute_data()
+    y_train = train_data["class"]
+    x_train = train_data.drop("class", axis=1)
+
+    y_test = test_data["class"]
+    x_test = test_data.drop("class", axis=1)
+
+    classification_scores = []
+    oob_errors = []
+    n_componenets = range(1,100)
+    for n in n_componenets:
+        print(n)
+        rfc = RandomForestClassifier(n_estimators=n, oob_score=True, n_jobs=4)
+        rfc.fit(x_train, y_train)
+        y_predict = rfc.predict(x_test)
+        classification_scores.append(accuracy_score(y_test, y_predict))
+        oob_errors.append(1-rfc.oob_score_)
+
+    #plot oob errorplot
+    min_score = min(oob_errors)
+    min_index = oob_errors.index(min_score)
+    print(oob_errors)
+    plt.plot(n_componenets,oob_errors,label = "RandomForestClassifier, max_features='sqrt'")
+    plt.plot(n_componenets[min_index],oob_errors[min_index],marker='X', ms=6)
+    plt.legend(loc="upper right")
+    plt.savefig("./plots/c_oob_errors.png")
+    plt.close()
+
+    max_score = max(classification_scores)
+    index = classification_scores.index(max_score)
+    n_tree = index+1
+    rfc = RandomForestClassifier(n_estimators=n_tree)
+    rfc.fit(x_train, y_train)
+    y_predict = rfc.predict(x_train)
+    #confusion Matrix Train Data
+    print("train accuracy score", accuracy_score(y_train, y_predict))
+    cm = confusion_matrix(y_train, y_predict, labels=["neg", "pos"])
+
+    plt.figure()
+    plot_confusion_matrix(cm, classes=["neg", "pos"],
+                          title='Confusion matrix, without normalization')
+    plt.savefig("./plots/c_train_confusion_matrix.png")
+    plt.close()
+
+    # confusion Matrix Test Data
+    y_predict = rfc.predict(x_test)
+    print("test accuracy score",accuracy_score(y_test, y_predict))
+
+    cm = confusion_matrix(y_test, y_predict, labels=["neg", "pos"])
+    plt.figure()
+    plot_confusion_matrix(cm, classes=["neg", "pos"],
+                          title='Confusion matrix, without normalization')
+    plt.savefig("./plots/c_test_confusion_matrix.png")
+    plt.close()
+
+    #ROC curve Train Data
+    Y_score = rfc.predict_proba(x_train)[:,1:]
+    plot_roc_curve(y_train,Y_score,"./plots/c_train_ROC_curve.png")
+    plt.close()
+
+    # ROC curve Test Data
+    Y_score = rfc.predict_proba(x_test)[:, 1:]
+    plot_roc_curve(y_test, Y_score, "./plots/c_test_ROC_curve.png")
+    plt.close()
+# ======================================================================================================================
+def d_random_forest_balanced():
+    train_data, test_data = b_i_impute_data()
+    y_train = train_data["class"]
+    x_train = train_data.drop("class", axis=1)
+
+    y_test = test_data["class"]
+    x_test = test_data.drop("class", axis=1)
+
+    classification_scores = []
+    oob_errors = []
+    n_componenets = range(1,100)
+    for n in n_componenets:
+        print(n)
+        rfc = RandomForestClassifier(n_estimators=n, oob_score=True, n_jobs=4, class_weight="balanced")
+        rfc.fit(x_train, y_train)
+        y_predict = rfc.predict(x_test)
+        classification_scores.append(accuracy_score(y_test, y_predict))
+        oob_errors.append(1-rfc.oob_score_)
+
+    #plot oob errorplot
+    min_score = min(oob_errors)
+    min_index = oob_errors.index(min_score)
+    print(oob_errors)
+    plt.plot(n_componenets,oob_errors,label = "RandomForestClassifier, max_features='sqrt'")
+    plt.plot(n_componenets[min_index],oob_errors[min_index],marker='X', ms=6)
+    plt.legend(loc="upper right")
+    plt.savefig("./plots/d_oob_errors.png")
+    plt.close()
+
+    max_score = max(classification_scores)
+    index = classification_scores.index(max_score)
+    n_tree = index+1
+    rfc = RandomForestClassifier(n_estimators=n_tree, class_weight="balanced")
+    rfc.fit(x_train, y_train)
+    y_predict = rfc.predict(x_train)
+    #confusion Matrix Train Data
+    print("train accuracy score", accuracy_score(y_train, y_predict))
+    cm = confusion_matrix(y_train, y_predict, labels=["neg", "pos"])
+
+    plt.figure()
+    plot_confusion_matrix(cm, classes=["neg", "pos"],
+                          title='Confusion matrix, without normalization')
+    plt.savefig("./plots/d_train_confusion_matrix.png")
+    plt.close()
+
+    # confusion Matrix Test Data
+    y_predict = rfc.predict(x_test)
+    print("test accuracy score",accuracy_score(y_test, y_predict))
+
+    cm = confusion_matrix(y_test, y_predict, labels=["neg", "pos"])
+    plt.figure()
+    plot_confusion_matrix(cm, classes=["neg", "pos"],
+                          title='Confusion matrix, without normalization')
+    plt.savefig("./plots/d_test_confusion_matrix.png")
+    plt.close()
+
+    #ROC curve Train Data
+    Y_score = rfc.predict_proba(x_train)[:,1:]
+    plot_roc_curve(y_train,Y_score,"./plots/d_train_ROC_curve.png")
+    plt.close()
+
+    # ROC curve Test Data
+    Y_score = rfc.predict_proba(x_test)[:, 1:]
+    plot_roc_curve(y_test, Y_score, "./plots/d_test_ROC_curve.png")
+    plt.close()
+
+
+import weka.core.converters as converters
+from weka.classifiers import Classifier, Evaluation
+from weka.core.classes import Random
+import weka.plot.classifiers as plcls
+def e_model_tree():
+    train_data, test_data = b_i_impute_data()
+    train_data.to_csv("./train_data.csv", index=False)
+    test_data.to_csv("./test_data.csv",index=False)
+
+    jvm.start()
+    train_data = converters.load_any_file("train_data.csv")
+    train_data.class_is_first()
+
+    test_data = converters.load_any_file("test_data.csv")
+    test_data.class_is_first()
+
+    print("1")
+    cls = Classifier(classname="weka.classifiers.trees.LMT")
+    print("2")
+    cls.build_classifier(train_data)
+    print("3")
+    evl = Evaluation(train_data)
+
+
+    evl.crossvalidate_model(cls, train_data, 5, Random(1))
+    print("Train Accuracy:", evl.percent_correct)
+    print("Train summary")
+    print(evl.summary())
+    print("Train class details")
+    print(evl.class_details())
+    print("Train confusion matrix")
+    print(evl.confusion_matrix)
+    plcls.plot_roc(evl, class_index=[0, 1], wait=True)
+    plt.savefig("./plots/e_train_roc_curve.png")
+
+    evl = Evaluation(test_data)
+    evl.test_model(cls, test_data)
+    print("Test Accuracy:", evl.percent_correct)
+    print("Test summary")
+    print(evl.summary())
+    print(" Testclass details")
+    print(evl.class_details())
+    print("Testconfusion matrix")
+    print(evl.confusion_matrix)
+    plcls.plot_roc(evl, class_index=[0, 1], wait=True)
+    plt.savefig("./plots/e_test_roc_curve.png")
+
+    # train_data, test_data = b_i_impute_data()
+    # y_train = train_data["class"]
+    # x_train = train_data.drop("class", axis=1)
+    #
+    # y_test = test_data["class"]
+    # x_test = test_data.drop("class", axis=1)
+    #
+
+
+
+
+if __name__ == '__main__':
+    e_model_tree()
